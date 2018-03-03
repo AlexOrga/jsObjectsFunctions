c0369598f62f5921cb0abccd90eae9b463c1b86b
Alex Orga <alexorga.dev@gmail.com>
2018-03-03 10:12:56 -0600

    added objects exercise

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
-console.log('hi');
+var faveAnimals = {
+    name: "zoe",
+    animal: "cat",
+};
+
+
+console.log("favAnimal", faveAnimals.animal);
+
+var instructorPets = [
+    {
+        instructorName: "Zoe",
+        petName: "Biff",
+        animalType: "evil cat",
+        isFavorite: false
+    },
+    {
+        instructorName: "Zoe",
+        petName: "Whiskey",
+        animalType: "cat",
+        isFavorite: true
+    },
+    {
+        instructorName: "Lauren",
+        petName: "Frankie",
+        animalType: "sweet dog",
+        isFavorite: true
+    },
+    {
+        instructorName: "Callan",
+        petName: "Seymour",
+        animalType: "one eyed dog",
+        isFavorite: true
+    },
+    {
+        instructorName: "Callan",
+        petName: "Wiley",
+        animalType: "destructive dog",
+        isFavorite: false
+    }
+];
+console.log("instructorPets", instructorPets);
+var getId = document.getElementById('pet-names');
+
+for (var i=0; i<instructorPets.length; i++){
+    getId.innerHTML += '<h1>' + instructorPets[i].petName + '</h1>';
+}
